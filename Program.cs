019fe09b8ae19eadea4d00612d5eac2e269ef281
Brandon <bhicks1305@gmail.com>
2021-02-23 21:30:06 -0500

    Started working on the stack exercise

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace StackExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
         }
     }
 }
---
+++ Stack.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        public void Push(object obj)
+        {
+            List<object> value = new List<object>();
+        }
+    }
+}
