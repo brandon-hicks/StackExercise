61d92a1e4b251c8d2bcf1c5d80f5d6ea9f7b5bbd
Brandon <bhicks1305@gmail.com>
2021-02-24 09:04:23 -0500

    still working on the stack exercise

--- Stack.cs
+++ Stack.cs
@@ -1,12 +1,32 @@
+using System;
 using System.Collections.Generic;
 
 namespace StackExercise
 {
     public class Stack
     {
+        private static List<object> value = new List<object>();
         public void Push(object obj)
         {
-            List<object> value = new List<object>();
+            if (obj == null)
+            {
+               throw new InvalidOperationException();
+            }
+            else
+                value.Add(obj);
+        }
+
+        public object Pop()
+        {
+            
+            for (int i = value.Count - 1; i >= 0; i--)
+            {
+                if(value[i] == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                return value[i];
+            }
         }
     }
 }
